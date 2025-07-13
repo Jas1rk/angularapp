1aa33bea8e685a9e1aabf92eaa7ff31968553698
Jasir K <jskjasir90@gmail.com>
2025-07-13 14:55:03 +0530

    intial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddTodo } from './add-todo/add-todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AddTodo],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todoapp');
