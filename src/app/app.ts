20d552feb72aa7a70a1429f17ec720c2f7d94db2
Jasir K <jskjasir90@gmail.com>
2025-07-13 15:18:55 +0530

    ui: for add task and list task

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTodo } from './add-todo/add-todo';
+import { ListTodo } from './list-todo/list-todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AddTodo],
+  imports: [RouterOutlet,AddTodo,ListTodo],
   templateUrl: './app.html',
 })
 export class App {
