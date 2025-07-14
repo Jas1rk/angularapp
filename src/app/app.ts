0985fba920623954f2008857b16ab50b37b9cec2
Jasir K <jskjasir90@gmail.com>
2025-07-14 17:01:55 +0530

    ui: list

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { AddTodo } from './add-todo/add-todo';
 import { ListTodo } from './list-todo/list-todo';
 
+type AddTod = {
+   id:number;
+  todo:string;
+  compleadted:boolean
+
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,AddTodo,ListTodo],
@@ -10,4 +17,10 @@ import { ListTodo } from './list-todo/list-todo';
 })
 export class App {
   protected readonly title = signal('todoapp');
+
+  allTodos:AddTod[] = []
+
+  onTodo(t:AddTod[]){
+     this.allTodos = t
+  }
 }
